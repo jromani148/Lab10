e3edbf5e9e6780c9fb522fa204d20fdae95da398
jromani148 <jr863@njit.edu>
2024-12-03 12:44:59 -0500

    not done

---
+++ lab10.js
@@ -0,0 +1,59 @@
+//Question 1
+function greetUser(){
+    let username = document.getElementById('username').value;
+    document.getElementById('heading-three').innerText = "Hello, " + username;
+    
+}
+    
+document.getElementById('button1').addEventListener('click', getInput)
+
+//Question 2
+function ChangeText(){
+    document.getElementById(`message`).innerText="Hello, World!";
+
+}
+
+//Question 3
+function Additon(){
+    document.getElementById(`1`).value;
+    document.getElementById(`2`).value;
+    document.getElementById(`result`)
+
+    if(!isNaN(`1`)&& !isNaN(`2`)){
+        sum = 1 +2;innerText= `result ${sum}`;
+
+
+    }
+    
+
+}
+
+//Question 4
+function newtitle(){
+    document.getElementById(`title`).value;
+    document.getElementById(`title`);
+
+    if()
+
+
+
+}
+
+
+
+
+
+
+//Question 5
+function AddNumbers(){
+    document.getElementById(`number`).innerText.value;
+    let a;
+    let b;
+     
+    document.getElementById('heading-two').innerText = (a + b);
+    document.getElementById('para1').innerHTML = "<h1>New Heading one</h1>"
+}
+
+//Question 6
+
+
